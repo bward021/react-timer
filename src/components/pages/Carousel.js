6af4767f06f18a444f9ccf356ea00b26c324e4b3
Brandon Ward <36583575+bward021@users.noreply.github.com>
2021-03-12 14:45:45 -0700

    Add Carousel and functionality to updated state in timer.

---
+++ src/components/pages/Carousel.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Carousel = (props) => {
+  const settings = {
+    infinite: true,
+    draggable: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    swipeToSlide: true,
+    arrows: false,
+    focusOnSelect: true,
+    centerMode: true
+  };
+  const renderSlides = (range) => {
+    return new Array(range + 1)
+      .fill("_")
+      .map((_, idx) => {
+        return idx.toString().padStart(2, "0");
+      })
+      .map((num, idx) => (
+        <div key={idx} className="number">
+          <h3>{num}</h3>
+        </div>
+      ));
+  };
+  return (
+    <div className="slider-container">
+      <div className="slider-header">
+        <h3>Hours</h3>
+        <h3>Minutes</h3>
+        <h3>Seconds</h3>
+      </div>
+      <div className="slider-wrapper">
+        <Slider
+          {...settings}
+          afterChange={(currentSlide) => props.setHours(currentSlide)}
+        >
+          {renderSlides(99)}
+        </Slider>
+        <div className="slider-colon">:</div>
+        <Slider
+          {...settings}
+          afterChange={(currentSlide) => props.setMinutes(currentSlide)}
+        >
+          {renderSlides(59)}
+        </Slider>
+        <div className="slider-colon">:</div>
+        <Slider
+          {...settings}
+          afterChange={(currentSlide) => props.setSeconds(currentSlide)}
+        >
+          {renderSlides(59)}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
+
+// Set correct numbers
+// - Hours 00 - 59
+// - Minutes 00 - 59
+// - Seconds 00 - 59
--- src/components/pages/Timer.js
+++ src/components/pages/Timer.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from "./Carousel";
 
 const Timer = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
   return (
-    <div>
-      <h1>Timer</h1>
-    </div>
+    <Carousel
+      setHours={setHours}
+      setMinutes={setMinutes}
+      setSeconds={setSeconds}
+    />
   );
 };
 
